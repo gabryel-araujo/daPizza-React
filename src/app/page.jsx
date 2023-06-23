a03daa5a3ceaaab135a09b40a922f789cc651331
Filipe Rodrigues <rodrigues.filipe@academico.ifpb.edu.br>
2023-06-23 17:32:27 -0300

    Página inicial completa

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,5 @@
 // import Image from "next/image";
-'use client';
+"use client";
 import NavBar from "@/components/NavBar";
 import Footer from "@/components/Footer";
 import { pizza_premium, pizza_tradicional, pizza_doce } from "./pizzas";
@@ -105,6 +105,51 @@ export default function Home() {
       <h1 className="text-slate-300 titulos">Doces</h1>
       <div id="doce" className="grid grid-cols-3 gap-56">
         {Doces}
+      </div>
+      <div className="alinhamento">
+        <h1 className="titulos" id="about-us">
+          Quem somos nós?
+        </h1>
+        <p className="lorem" id="descricao-pizzaria">
+          Há mais de 5 anos no mercado, nos tornamos referência quando o assunto
+          <br />
+          é qualidade, preço acessível e acima de tudo confiança. Nosso time de
+          <br />
+          profissionais são treinados e capacitados para lhe oferecer a melhor
+          <br />
+          experiência que você já teve em um atendimento. Venha nos conhecer!
+        </p>
+      </div>
+      <div id="about">
+        <img src="/pizza.png" alt="imagem de pizza" id="pizza-1" />
+        <div id="div-video" className="alinhamento">
+          <h1 id="titulo-apresentacao">Conheça o nosso preparo</h1>
+          <iframe
+            id="video"
+            width={560}
+            height={315}
+            src="https://www.youtube.com/embed/T3AHBe0I0yc"
+            title="YouTube video player"
+            frameBorder={0}
+            allow="accelerometer; autoplay; 
+          clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen=""
+          ></iframe>
+        </div>
+      </div>
+      <div className="localizacao-mapa">
+        <h2 className="titulos" id="localizacao">
+          Nossa Localização
+        </h2>
+        <iframe
+          id="mapa"
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d63342.6764697195!2d-34.939388268750044!3d-7.135564299999973!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x7ace810852ae4f1%3A0xaf5445223401f2bb!2sIFPB%20-%20Campus%20Jo%C3%A3o%20Pessoa!5e0!3m2!1sen!2sbr!4v1666495722215!5m2!1sen!2sbr"
+          width={600}
+          height={450}
+          allowFullScreen=""
+          loading="lazy"
+          referrerPolicy="no-referrer-when-downgrade"
+        />
       </div>
       <Footer />
     </>
