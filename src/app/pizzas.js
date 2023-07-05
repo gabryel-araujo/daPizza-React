82b6e42c2dbb5117b5224744501cec03feabb2a6
Filipe Rodrigues <rodrigues.filipe@academico.ifpb.edu.br>
2023-07-05 11:14:40 -0300

    Alterações na descrição

--- src/app/pizzas.js
+++ src/app/pizzas.js
@@ -5,7 +5,7 @@ const pizza_premium = [
     image: "/Queijo Especial.jpeg",
     name: "Queijo Especial",
     description:
-      "Farinha italiana,<br>molho à base de creme<br>de parmesão, mussarela,<br>DorBlu, parmesão,<br>noz e cranberry.<br>",
+      "Farinha italiana,molho à base de creme de parmesão, mussarela,DorBlu, parmesão,noz e cranberry.",
     value: 40.0,
   },
   {
@@ -14,7 +14,7 @@ const pizza_premium = [
     image: "/pepperoni.jpeg",
     name: "Pepperoni",
     description:
-      "Farinha italiana, mussarela,<br>molho de tomate integral,<br>pepperoni premium, orégano.<br>",
+      "Farinha italiana, mussarela, molho de tomate integral, pepperoni premium, orégano.",
     value: 40.0,
   },
   {
@@ -23,7 +23,7 @@ const pizza_premium = [
     image: "/à moda da casa.jpeg",
     name: "À Moda da Casa",
     description:
-      "Farinha italiana,<br>molho de tomate integral,<br>mussarela, carne bovina,<br>calabresa, linguiça de caça,<br>pimentão, cebola roxa,<br>molho barbecue e orégano.<br>  ",
+      "Farinha italiana, molho de tomate integral, mussarela, carne bovina,calabresa, linguiça de caça, pimentão, cebola roxa,molho barbecue e orégano.  ",
     value: 40.0,
   },
   {
@@ -32,7 +32,7 @@ const pizza_premium = [
     image: "/bacon.jpeg",
     name: "Bacon",
     description:
-      "Farinha italiana, molho de<br>tomate integral, mussarela,<br>dorblu, bacon fatiado,<br>pimenta, orégano.<br> ",
+      "Farinha italiana, molho detomate integral, mussarela,dorblu, bacon fatiado,pimenta, orégano. ",
     value: 40.0,
   },
   {
@@ -41,7 +41,7 @@ const pizza_premium = [
     image: "/marguerita.jpeg",
     name: "Marguerita",
     description:
-      "Farinha italiana, molho<br>de tomate integral,<br>mussarela, tomate, molho<br>pesto, orégano.<br>",
+      "Farinha italiana, molhode tomate integral, mussarela, tomate, molhopesto, orégano.",
     value: 40.0,
   },
   {
@@ -50,7 +50,7 @@ const pizza_premium = [
     image: "/Salmão.jpeg",
     name: "Salmão",
     description:
-      "Farinha italiana, molho<br>de tomate integral,<br>mussarela, salmão, tomate,<br>espinafre, Dorblu, alho,<br>limão, azeitonas, orégano.<br>",
+      "Farinha italiana, molhode tomate integral, mussarela, salmão, tomate, espinafre, Dorblu, alho, limão, azeitonas, orégano.",
     value: 40.0,
   },
 ];
@@ -62,7 +62,7 @@ const pizza_tradicional = [
     image: "/calabresa.png",
     name: "Calabresa",
     description:
-      "Queijo mussarela, fatias finas<br>de cebola, rodelas de calabresa,<br>azeitona preta e orégano.<br>",
+      "Queijo mussarela, fatias finasde cebola, rodelas de calabresa,azeitona preta e orégano.",
     value: 30.0,
   },
   {
@@ -71,7 +71,7 @@ const pizza_tradicional = [
     image: "/portuguesa.webp",
     name: "Portuguesa",
     description:
-      "Queijo mussarela , presunto<br>fatiado, ovos fatiados,<br>cebola roxa, pimentões<br>vermelhos e amarelos<br>azeitona preta e orégano.<br> ",
+      "Queijo mussarela, presunto fatiado, ovos fatiados, cebola roxa, pimentões vermelhos e amarelos, azeitona preta e orégano. ",
     value: 30.0,
   },
   {
@@ -80,7 +80,7 @@ const pizza_tradicional = [
     image: "/4-queijos.jpg",
     name: "4 Quejjos<",
     description:
-      "Queijo mussarela, gorgonzola,<br>provolone, requeijão,<br>azeitona preta e orégano.<br>",
+      "Queijo mussarela, gorgonzola, provolone, requeijão, azeitona preta e orégano.",
     value: 30.0,
   },
   {
@@ -89,7 +89,7 @@ const pizza_tradicional = [
     image: "/brocolis.png",
     name: "Brócolis",
     description:
-      "Queijo mussarela fresco,<br>brócolis, alho frito,<br>orégano, molho especial<br>de parmesão e queijo parmesão.<br>",
+      "Queijo mussarela fresco, brócolis, alho frito,orégano, molho especialde parmesão e queijo parmesão.",
     value: 30.0,
   },
   {
@@ -98,7 +98,7 @@ const pizza_tradicional = [
     image: "/frango-catupiry.png",
     name: "Frango Com Catupiry&reg;",
     description:
-      "Queijo mussarela, frango assado<br>em cubos, com catupirry&reg;,<br>milho, bacon, azeitona<br>preta e orégano.<br>",
+      "Queijo mussarela, frango assado em cubos, com catupirry, milho, bacon, azeitona preta e orégano.",
     value: 30.0,
   },
   {
@@ -107,7 +107,7 @@ const pizza_tradicional = [
     image: "/carne-de-sol.png",
     name: "Carne de Sol",
     description:
-      "Queijo mussarela, carne de sol<br>desfiada, finas fatias de cebola,<br>Requeijão cremoso, azeitona<br>preta e orégano.<br> ",
+      "Queijo mussarela, carne de soldesfiada, finas fatias de cebola, Requeijão cremoso, azeitona preta e orégano. ",
     value: 30.0,
   },
   {
@@ -116,7 +116,7 @@ const pizza_tradicional = [
     image: "/frango-ao-barbecue.png",
     name: "Frango com Barbecue",
     description:
-      "Queijo mussarela, frango<br>assado desfiado, finas<br>fatias de bacon, molho barbecue,<br>azeitona preta e orégano.<br>",
+      "Queijo mussarela, frangoassado desfiado, finasfatias de bacon, molho barbecue,azeitona preta e orégano.",
     value: 30.0,
   },
   {
@@ -125,7 +125,7 @@ const pizza_tradicional = [
     image: "/frango-cheddar.png",
     name: "Frango com Cheddar",
     description:
-      "Queijo mussarela fresco,<br>frango desfiado temperado<br>com ervas finas e<br>cobertura de Cheddar.",
+      "Queijo mussarela fresco, frango desfiado temperado com ervas finas ecobertura de Cheddar.",
     value: 30.0,
   },
   {
@@ -134,7 +134,7 @@ const pizza_tradicional = [
     image: "/picanha.png",
     name: "Picanha",
     description:
-      "Queijo mussarela fresco,<br>fatias de picanha temperada<br>com ervas finas, sal, <br>creme de alho e manjericão.<br>",
+      "Queijo mussarela fresco, fatias de picanha temperadacom ervas finas, sal, creme de alho e manjericão.",
     value: 30.0,
   },
 ];
@@ -145,7 +145,7 @@ const pizza_doce = [
     category: "doce",
     image: "/chocolate.png",
     name: "Chocolate ao Leite",
-    description: "Chocolate ao leite gratinado.<br>",
+    description: "Chocolate ao leite gratinado.",
     value: 35.0,
   },
   {
@@ -153,7 +153,7 @@ const pizza_doce = [
     category: "doce",
     image: "/chocolate-branco.png",
     name: "Chocolate Branco",
-    description: "Chocolate branco gratinado.<br>",
+    description: "Chocolate branco gratinado.",
     value: 35.0,
   },
   {
@@ -161,7 +161,7 @@ const pizza_doce = [
     category: "doce",
     image: "/dois-amores.png",
     name: "Dois Amores",
-    description: "Chocolate ao leite e  chocolate branco.<br>",
+    description: "Chocolate ao leite e  chocolate branco.",
     value: 35.0,
   },
 ];
